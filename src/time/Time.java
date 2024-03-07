eaebe6225d3b339cb70f5a754a90402d39c14546
Suresh <sures@CALAP2366.home>
2024-03-07 03:09:02 -0500

    test commit from eclipse 2

--- src/time/Time.java
+++ src/time/Time.java
@@ -24,6 +24,7 @@ public class Time
 	public static void main(String[] args) {
 		int totalSeconds = getTotalSeconds("10:10:10");
 		System.out.println("Total Seconds = "+totalSeconds);
+		//test
 		}
 
 	public static int getTotalSeconds(String time)throws NumberFormatException, StringIndexOutOfBoundsException {
